9773b7e5c645128d1691f80bb02aeef75e25948b
Suraj Hanvate <surajhanvate999@gmail.com>
2024-02-28 15:31:26 +0530

    Implemented CORS policy

--- backend/src/main/java/com/suraj/config/AppConfig.java
+++ backend/src/main/java/com/suraj/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.suraj.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -18,12 +25,32 @@ public class AppConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(Authorize -> Authorize
-				.requestMatchers("/api/**").authenticated()
-				.anyRequest().permitAll())
+						.requestMatchers("/api/**").authenticated()
+						.anyRequest().permitAll())
 				.addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
-				.csrf(AbstractHttpConfigurer::disable);
+				.csrf(AbstractHttpConfigurer::disable)
+				.cors(cors -> cors.configurationSource(configurationSource()));
 
 		return http.build();
+	}
+
+	private CorsConfigurationSource configurationSource() {
+		return new CorsConfigurationSource() {
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				CorsConfiguration configuration = new CorsConfiguration();
+				configuration.setAllowedOrigins(Arrays.asList(
+						"http://localhost:3000"
+				));
+				configuration.setAllowedMethods(Collections.singletonList("*"));
+				configuration.setAllowCredentials(true);
+				configuration.setAllowedHeaders(Collections.singletonList("*"));
+				configuration.setExposedHeaders(List.of("Authorization"));
+				configuration.setMaxAge(3600L);
+				
+				return configuration;
+			}
+		};
 	}
 
 	@Bean
